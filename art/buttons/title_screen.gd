c11a1db03e9022671594557a5b92441a82243de9
SebastianCapuyanNSJ <capuyanj@uci.edu>
2026-01-26 11:42:17 -0800

    Made place placeholder button textures, and made titleScreen Scene

---
+++ art/buttons/title_screen.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn") # Replace with function body.
+
+func _on_exit_pressed() -> void:
+	get_tree().quit() # Replace with function body.

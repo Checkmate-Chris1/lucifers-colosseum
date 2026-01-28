bfcbbfb114f6026814cc888228b8ff786127f8f3
Harrison Tran <harrisontran357@gmail.com>
2026-01-27 22:37:47 -0800

    Tormented Soul Chases Player (#3)
    * tormented soul saved as scene
    
    * t soul scene
    
    * tormented capsule chases player
    
    * soul turns toward player
    
    * basic avoidance
    
    * Renamed tormented soul in scene tree
    
    ---------
    
    Co-authored-by: CheckmateChris1 <80181605+Checkmate-Chris1@users.noreply.github.com>

---
+++ entities/enemies/tormentedSoul/tormentedSoul.gd
@@ -0,0 +1,22 @@
+extends CharacterBody3D
+
+const SPEED = 2.0
+var player = null
+
+@onready var nav_agent = $NavigationAgent3D
+@export var player_parent : NodePath
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var player_root = get_node(player_parent)
+	player = player_root.get_node("Player")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:	
+	velocity = Vector3.ZERO
+	nav_agent.target_position = player.global_position
+	var next_point = nav_agent.get_next_path_position()
+	look_at(Vector3(player.global_position.x, player.global_position.y, player.global_position.z), Vector3.UP)
+	velocity = (next_point - global_position).normalized() * SPEED
+	
+	move_and_slide()

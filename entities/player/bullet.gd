2eb33769e8e060f14f0441d02dba56fdc775d969
Ayaan Mupparthi <120342231+AyaanMupparthi@users.noreply.github.com>
2026-02-02 00:35:40 -0800

    Gun ammo, bullet projectiles
    Created a Gun as a child of the Player. It keeps track of the amount of ammo left in the gun, as well a timer to control fire rate. There is also now a bullet scene. When the player left-clicks, a new bullet scene is instantiated and the bullet (which has a script telling it to move in a straight line) spawns into the world. As of right now, the bullet spawns from the center of the arena and moves in a fixed direction until colliding with the wall. In the future, I want to hook up the bullets to fire in the direction of the player camera. Additionally, I want to have a delay when reloading (which is now done instantaneously when R is pressed).

---
+++ entities/player/bullet.gd
@@ -0,0 +1,19 @@
+extends CharacterBody3D
+
+@export var speed := 50.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+	# I don't really understand how these lines work - Ayaan
+	var velocity = -global_transform.basis.z * speed
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		# handle collision
+		pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ entities/player/gun.gd
@@ -0,0 +1,49 @@
+extends Node3D
+
+@export var bullet_scene: PackedScene
+@export var fire_rate := 0.2
+
+
+var timer := Timer.new()
+var ready_to_shoot := true
+@export var max_ammo := 10
+var ammo := max_ammo
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_child(timer)
+	timer.wait_time = fire_rate
+	timer.one_shot = true
+	timer.timeout.connect(_on_timer_end)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("fire_weapon"):
+		fire()
+	elif Input.is_action_just_pressed("reload_weapon"):
+		reload()
+
+func fire() -> void:
+	if not ready_to_shoot:
+		return
+	if ammo > 0:
+		create_bullet()
+		ammo -= 1
+		ready_to_shoot = false
+		timer.start()
+	else:
+		print('out of ammo')
+
+func create_bullet() -> void:
+	var bullet = bullet_scene.instantiate()
+	get_tree().root.add_child(bullet)
+
+
+func reload() -> void:
+	ammo = max_ammo
+	print('reloaded')
+
+func _on_timer_end() -> void:
+	ready_to_shoot = true

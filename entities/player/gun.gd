d2284d2373b41a81e88819f188b5f8ca0244fb03
Ayaan Mupparthi <120342231+AyaanMupparthi@users.noreply.github.com>
2026-02-05 23:06:41 -0800

    Reload cooldown, raycast queries,
    Added cooldown for reloading, as opposed to the instantaneous cooldown before. Added enum that allows to define a weapon type as projectile (shooting physical bullets like before) and a new type called raycast (shooting an invisible ray from the camera and seeing what it collides with). As of now, the information is collected (line 84), but nothing is done with it. Weapon type can be changed on line 13. Also, variables at the top have been better organized.

--- entities/player/gun.gd
+++ entities/player/gun.gd
@@ -1,27 +1,41 @@
 extends Node3D
 
+
+# Projectile refers to bullets shooting at finite speeds
+# Raycast refers to hitscan "lasers" (most weapons)
+# Can also add "conical" for flamethrowers
+enum WeaponType {PROJECTILE, RAYCAST}
+
 @export var bullet_scene: PackedScene
-@export var fire_rate := 0.2
+@export var fire_delay := 0.2
+@export var reload_delay := 1
+@export var max_ammo := 10
+@export var weapon_type := WeaponType.RAYCAST
+@export var range := 99 # larger than the diameter of the arena
 
 var player: Node3D
-
-
-var timer := Timer.new()
+var fire_timer := Timer.new()
+var reload_timer := Timer.new()
 var ready_to_shoot := true
-@export var max_ammo := 10
 var ammo := max_ammo
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_child(timer)
-	timer.wait_time = fire_rate
-	timer.one_shot = true
-	timer.timeout.connect(_on_timer_end)
 	player = get_parent()
+	
+	add_child(fire_timer)
+	fire_timer.wait_time = fire_delay
+	fire_timer.one_shot = true
+	fire_timer.timeout.connect(_on_fire_timer_end)
+	
+	add_child(reload_timer)
+	reload_timer.wait_time = reload_delay
+	reload_timer.one_shot = true
+	reload_timer.timeout.connect(_on_reload_timer_end)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("fire_weapon"):
 		fire()
@@ -31,25 +45,52 @@ func _process(delta: float) -> void:
 func fire() -> void:
 	if not ready_to_shoot:
 		return
-	if ammo > 0:
-		create_bullet()
-		ammo -= 1
-		ready_to_shoot = false
-		timer.start()
-	else:
+	if ammo <= 0:
+		# this should be replaced with some in-game notification
+		# or maybe this auto-triggers a reload
 		print('out of ammo')
+		return
+	
+	if weapon_type == WeaponType.PROJECTILE:
+		shoot_bullet()
+	elif weapon_type == WeaponType.RAYCAST:
+		shoot_ray()
+		
+	ammo -= 1
+	ready_to_shoot = false
+	fire_timer.start()
 
-func create_bullet() -> void:
+func reload() -> void:
+	ready_to_shoot = false
+	reload_timer.start()
+
+func shoot_bullet() -> void:
 	var bullet = bullet_scene.instantiate()
 	get_tree().root.add_child(bullet)
 	bullet.global_transform = player.get_node('CameraController/Camera').global_transform
-	#bullet.global_transform = player.global_transform
-	#print(player.get_child(2).get_child(0).global_transform)
+
+func shoot_ray() -> void:
+	var space_state = get_world_3d().direct_space_state
+	var cam = player.get_node('CameraController/Camera')
+	var mouse_pos = get_viewport().size / 2 # center of screen, i.e. the crosshairs
+	
+	var origin = cam.global_position
+	var end = origin + cam.project_ray_normal(mouse_pos) * range
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+	
+	# this returns a dictionary of info on the point that the ray collided with, including its 
+	# global position and the type of object the collision was with
+	var result = space_state.intersect_ray(query)
+	
+	
 
 
-func reload() -> void:
+
+
+func _on_fire_timer_end() -> void:
+	ready_to_shoot = true
+
+func _on_reload_timer_end() -> void:
 	ammo = max_ammo
-	print('reloaded')
-
-func _on_timer_end() -> void:
 	ready_to_shoot = true

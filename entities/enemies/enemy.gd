3c405d661f6953086eb0d4ac6a57cca23ee49914
CheckmateChris1 <checkdoesgames@gmail.com>
2026-02-09 16:28:55 -0800

    Abstracted TS & adjusted settings

--- entities/enemies/enemy.gd
+++ entities/enemies/enemy.gd
@@ -7,8 +7,13 @@ class_name Enemy
 @export var ATTACK_DAMAGE: float = 15.0 ## The base attack damage
 @export var ATTACK_DELAY: float = 2.0 ## The delay between attacks in seconds
 @export var attack_collision_box: Area3D
+@export var enemy_model: Node3D
 
+@onready var animation_player: AnimationPlayer = enemy_model.get_node("AnimationPlayer")
 var attack_ready := true ## If another attack is ready
+
+func _ready() -> void:
+	animation_player.play("Idle") # idle
 
 func _init() -> void:
 	died.connect(queue_free) # All enemies should be removed from the scene upon death
@@ -19,5 +24,7 @@ func attack():
 		for entity in within_attack:
 			if entity is Player:
 				entity.damage(ATTACK_DAMAGE)
+				# animation_player.play("attack")
+				# Add this when all enemies actually have an attack animation
 				attack_ready = false
 				get_tree().create_timer(ATTACK_DELAY).timeout.connect(func(): attack_ready = true)
--- entities/enemies/tormentedSoul/tormentedSoul.gd
+++ entities/enemies/tormentedSoul/tormentedSoul.gd
@@ -2,14 +2,7 @@ extends Enemy
 
 
 @onready var nav_agent = $NavigationAgent3D
-@export var glb_import: Node3D
 @onready var player = get_tree().get_first_node_in_group('player')
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	# Animations
-	var animation_player: AnimationPlayer = glb_import.get_node("AnimationPlayer")
-	animation_player.play("Idle") # idle
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:

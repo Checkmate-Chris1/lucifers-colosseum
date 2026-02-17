a57729301d9ea5ebf45bd6f4d2300f9110b89182
Ayaan Mupparthi <120342231+AyaanMupparthi@users.noreply.github.com>
2026-02-17 15:23:55 -0800

    Added functionality for changing songs
    When the apostrophe (') is pressed, the music changes to a different stream. In the future, this would be triggered by a global signal rather than a key pressed. For the future, any AudioStream that is imported needs to have looping enabled.

--- music_manager.gd
+++ music_manager.gd
@@ -1,12 +1,24 @@
 extends AudioStreamPlayer
 
+@export var streams = [load('res://audio/musicPlaceholder.mp3'), load('res://audio/musicPlaceholder2.mp3')]
+var i = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	stream = streams[i]
+	play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause_music"):
-		self.set_stream_paused(not self.get_stream_paused())
+		stream_paused = not stream_paused
+	elif Input.is_action_just_pressed("change_music"):
+		_change_song()
+
+func _change_song():
+	i += 1
+	if i >= len(streams):
+		i = 0
+	stream = streams[i]
+	play()

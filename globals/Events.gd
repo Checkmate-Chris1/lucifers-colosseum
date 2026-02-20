11507fcdf003d5021f64c57927d70e1ce10a247d
CheckmateChris1 <80181605+Checkmate-Chris1@users.noreply.github.com>
2026-02-20 00:08:06 -0800

    Feature/wave system (#28)
    * Cleaned up EnemySpawner
    
    * Read desc (WIP)
    
    - Out-of-bound hitbox made larger
    - Enemies don't collide with map walls
    - Enemies can walk through gates but not player
    - Wave system attempts to spawn enemies at game start (WIP)
    
    * Enemies spawn at game start
    
    * WIP commit
    
    taking a break
    
    * Fixed bug with EnemySpawner type confusing with Marker3D
    
    I don't think I actually changed anything but it fixed it?
    
    * Waves function
    
    * Pausing stops waves
    
    * Always takes 5 seconds to spawn wave

--- globals/Events.gd
+++ globals/Events.gd
@@ -5,8 +5,7 @@ extends Node
 
 signal respawn ## Respawn the player back to the starting position
 signal start_game ## Starts/restarts the main game
+signal wave_start ## Triggered after wave grace period
+signal wave_end ## Triggered when all enemies die or wave timer ends, whichever comes first
 signal toggle_pause ## Triggered by pause UI
 signal game_over ## Signal when the game is over by death
-
-func _init() -> void:
-	game_over.connect(func(): print("Game ended"))
---
+++ levels/common/wave_system/wave_system.gd
@@ -0,0 +1,54 @@
+extends Node3D
+
+
+@export var wave_length: float = 25.0
+@export var wave_grace_period: float = 10.0
+@export var enemy_weights: Dictionary[PackedScene, float]
+
+var enemy_spawners: Array[EnemySpawner] = []
+
+func _ready() -> void:
+	Events.wave_end.connect(_on_wave_end)
+	_update_spawners()
+	spawn_wave(3)
+
+func spawn_wave(enemy_count: int) -> void:
+	GameState.wave_number += 1
+	Events.wave_start.emit()
+	get_tree().create_timer(wave_length, false).timeout.connect(Events.wave_end.emit)
+	for __ in range(enemy_count):
+		_spawn_enemy()
+		await get_tree().create_timer(5.0/enemy_count, false).timeout
+		
+func _on_wave_end() -> void:
+	await get_tree().create_timer(wave_grace_period, false).timeout
+	# Enemy count goes 3, 5, 6, 7, 8, etc.
+	spawn_wave(3+GameState.wave_number)
+		
+func _spawn_enemy() -> void:
+	var spawner_index = randi() % len(enemy_spawners)
+	var selected_enemy = _get_from_weighted_values(enemy_weights)
+	enemy_spawners[spawner_index].spawn_enemy(selected_enemy)
+
+func _get_from_weighted_values(weight_dict: Dictionary):
+	var total_weight = _sum(weight_dict.values())
+
+	var pick = randf() * total_weight
+	var current_weight = 0.0
+
+	for enemy_scene in weight_dict.keys():
+		current_weight += weight_dict[enemy_scene]
+		if current_weight >= pick:
+			return enemy_scene
+
+func _update_spawners() -> void:
+	enemy_spawners = []
+	for child in get_children():
+		if child is EnemySpawner:
+			enemy_spawners.append(child)
+
+func _sum(array: Array):
+	var sum = 0
+	for item in array:
+		sum += item
+	return sum

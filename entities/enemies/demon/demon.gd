5322f66a1ef7ad6999d84240adedfeeca0dcfccd
Akhil Doddapaneni <akhil07d@gmail.com>
2026-02-21 13:53:10 -0800

    Added basic demon code
    Really just a copy of the tormented soul code, with the extra updating of posirioning the demon's y axis

--- entities/enemies/demon/demon.gd
+++ entities/enemies/demon/demon.gd
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 extends Node
 
 
@@ -9,3 +10,29 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	pass
+=======
+extends Enemy
+
+
+@onready var nav_agent = $NavigationAgent3D
+@onready var player = get_tree().get_first_node_in_group('player')
+
+const FLOATING_HEIGHT := 10
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if player != null:
+		velocity = Vector3.ZERO
+		nav_agent.target_position = player.global_position
+		var next_point = nav_agent.get_next_path_position()
+		look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+		velocity = (next_point - global_position).normalized() * SPEED
+		
+		global_position.y = FLOATING_HEIGHT
+		
+		attack()
+		move_and_slide()
+	else:
+		# only occurs when exiting game
+		queue_free()
+>>>>>>> Stashed changes

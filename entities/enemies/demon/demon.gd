dc21fa6cf90ec0dcd8c159436d610e6773ff5bd8
Akhil Doddapaneni <akhil07d@gmail.com>
2026-02-23 12:56:15 -0800

    Added Demon
    Needs some testing:
    also needs some fixing as my current version is wrong.

--- entities/enemies/demon/demon.gd
+++ entities/enemies/demon/demon.gd
@@ -1,16 +1,3 @@
-<<<<<<< Updated upstream
-extends Node
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	pass
-=======
 extends Enemy
 
 
@@ -21,18 +8,13 @@ const FLOATING_HEIGHT := 10
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if player != null:
-		velocity = Vector3.ZERO
-		nav_agent.target_position = player.global_position
-		var next_point = nav_agent.get_next_path_position()
-		look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
-		velocity = (next_point - global_position).normalized() * SPEED
-		
-		global_position.y = FLOATING_HEIGHT
-		
-		attack()
-		move_and_slide()
-	else:
-		# only occurs when exiting game
-		queue_free()
->>>>>>> Stashed changes
+	velocity = Vector3.ZERO
+	nav_agent.target_position = player.global_position
+	var next_point = nav_agent.get_next_path_position()
+	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	velocity = (next_point - global_position).normalized() * SPEED
+	
+	global_position.y = FLOATING_HEIGHT
+	
+	attack()
+	move_and_slide()

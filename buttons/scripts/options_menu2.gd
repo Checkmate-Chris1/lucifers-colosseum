55866bbdfd9c4b34301dfe03203c04775e8b09b8
Ayaan Mupparthi <120342231+AyaanMupparthi@users.noreply.github.com>
2026-02-23 18:27:11 -0800

    Music volume slider is now functional
    For the future, volume should stay below 0 dB.

--- buttons/scripts/options_menu2.gd
+++ buttons/scripts/options_menu2.gd
@@ -7,4 +7,7 @@ func _on_sfx_slider_value_changed(value: float) -> void:
 	pass
 	
 func _on_music_slider_value_changed(value: float) -> void:
-	pass
+	BackgroundMusicPlayer.volume_db = 30 * value - 30
+	if value == 0:
+		BackgroundMusicPlayer.volume_db = -80
+	

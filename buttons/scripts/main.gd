ca408b38864269394cbd4daad0d8229560761714
Harrison Tran <harrisontran357@gmail.com>
2026-02-26 22:20:10 -0800

    health bar increases on change

--- buttons/scripts/main.gd
+++ buttons/scripts/main.gd
@@ -6,3 +6,5 @@ extends Node3D
 func _ready():
 	health_bar.init_health(player.current_health, player.HEALTH)
 	player.health_changed.connect(health_bar._set_health)
+	if player.has_signal("max_health_changed"):
+		player.max_health_changed.connect(health_bar._set_max_health)

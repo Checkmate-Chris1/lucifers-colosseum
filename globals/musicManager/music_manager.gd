9c2792291ac855cf6910eb22c13605ea92b6352e
CheckmateChris1 <80181605+Checkmate-Chris1@users.noreply.github.com>
2026-03-02 23:10:29 -0800

    Added song cross-fading

--- globals/musicManager/music_manager.gd
+++ globals/musicManager/music_manager.gd
@@ -3,7 +3,8 @@ extends AudioStreamPlayer
 const min_volume := -30
 const volume_range := 30
 
-@export var streams = [load('res://audio/Hell\'s Fury.wav'), load('res://audio/Hell\'s Fury 2.wav')]
+@export var streams: Array[AudioStream] = [load('res://audio/Hell\'s Fury.wav'), load('res://audio/Hell\'s Fury 2.wav')]
+@onready var transition_player: AudioStreamPlayer = $Transitioner
 var i = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -12,12 +13,13 @@ func _ready() -> void:
 	Events.change_sound_volume.connect(_update_volume)
 	Events.wave_start.connect(_on_wave_start)
 	play()
+	await get_tree().create_timer(1).timeout
+	_change_song()
 
 
 func _on_wave_start():
 	print(GameState.wave_number)
 	if GameState.wave_number == 10:
-		volume_db = -10 # Lvl. 2 wave music is kinda loud
 		_change_song()
 
 
@@ -29,6 +31,22 @@ func _process(delta: float) -> void:
 
 func _change_song():
 	i += 1
+	
+	# Transitioning between songs
+	transition_player.stream = streams[i]
+	transition_player.volume_linear = 0
+	transition_player.play()
+	var t = 0
+	while t < 1:
+		t += 0.01
+		volume_linear = 1-t
+		transition_player.volume_linear = t
+		await get_tree().create_timer(0.02).timeout
+	stream = streams[i]
+	volume_linear = 1
+	play(2)
+	transition_player.stop()
+		
 	if i >= len(streams):
 		i = 0
 	stream = streams[i]

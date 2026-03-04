48a94eee306ac289544b3eff73e3be4b55df2b0a
Akhil Doddapaneni <akhil07d@gmail.com>
2026-03-03 18:08:33 -0800

    Fixed wave ending early
    off of mostly random chance, some enemies would run their checks for health <= 0 MULTIPLE TIMES, and thus emit their death MULTIPLE TIMES, causing us to lower our count of enemies MULTIPLE TIMES per enemy.
    
    added into entity.gd a death_emitted event, which allows each entity to only emit its death ONCE

--- levels/common/wave_system/wave_system.gd
+++ levels/common/wave_system/wave_system.gd
@@ -16,8 +16,10 @@ func _ready() -> void:
 	#print('ready')
 
 func _enemy_death_process():
-	#print('e_death')
+	print('e_death')
+	print(enemy_count)
 	enemy_count -= 1
+	print(enemy_count)
 	if enemy_count == 0:
 		_on_wave_end()
 	

f3a8f57aef53f854152c79c6c72219ae3f708987
Akhil Doddapaneni <akhil07d@gmail.com>
2026-03-06 18:04:30 -0800

    Angered Soul Completed
    BUG: since the wall also counts as layer 2, the angered soul can dash through the walls, but would not be able to walk through the gate during normal spawning.

--- entities/enemies/angeredSoul/angeredSoul.gd
+++ entities/enemies/angeredSoul/angeredSoul.gd
@@ -1,68 +1,58 @@
 extends Enemy
 
-
 @onready var nav_agent = $NavigationAgent3D
 @onready var player = get_tree().get_first_node_in_group('player')
 
-var DASH_MULTIPLIER := 5
 var BASE_VELOCITY : float = 6.0
+var DASH_MULTIPLIER := 5
 
 var dash_ready : bool = true
+var is_dashing : bool = false
+var dash_direction : Vector3
+var dash_speed : float
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if player != null:
-		velocity = Vector3.ZERO
-		nav_agent.target_position = player.global_position
-		var next_point = nav_agent.get_next_path_position()
+	if player == null:
+		queue_free()
+		return
+
+	nav_agent.target_position = player.global_position
+	var next_point = nav_agent.get_next_path_position()
+
+	if is_dashing:
+		velocity = dash_direction * dash_speed
+	else:
 		look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
-		velocity = (next_point - global_position).normalized() * SPEED
-		
+		velocity = (next_point - global_position).normalized() * BASE_VELOCITY
+
 		if dash_ready:
 			_dash()
-		
-		
-		attack()
-		move_and_slide()
-	else:
-		# only occurs when exiting game
-		queue_free()
-	
-	
-func _get_dash_direction() -> Vector3:
-	velocity = velocity * 5
-	var rotate_range = randf_range(-30, 30)
-	var target_var = "x" if randi_range(0, 1) == 1 else "z"
-	var ret_val : Vector3
-	if target_var == "x":
-		ret_val = Vector3(player.global_position.x + rotate_range, player.global_position.y, player.global_position.z)
-	elif target_var == "z":
-		ret_val = Vector3(player.global_position.x, player.global_position.y, player.global_position.z + rotate_range)
 
-	return ret_val
-		
-	
+	attack()
+	move_and_slide() 
+
+func _get_dash_direction() -> Vector3:
+	var to_player = (player.global_position - global_position).normalized()
+	var angle = deg_to_rad(randf_range(-30, 30))
+	var dash_dir = to_player.rotated(Vector3.UP, angle).normalized()
+	return dash_dir
+
 func _dash():
 	dash_ready = false
-	
-	# play a little animation to show its about to dash perhaps?
-	#await get_tree().create_timer(0.2).timeout
-	
-	
-	var dash_direction : Vector3 = _get_dash_direction()
-	look_at(dash_direction, Vector3.UP)
-	
-	velocity = velocity * DASH_MULTIPLIER
+	is_dashing = true
+
+	dash_direction = _get_dash_direction()
+	dash_speed = BASE_VELOCITY * DASH_MULTIPLIER
+	look_at(global_position + dash_direction, Vector3.UP)
+	set_collision_mask_value(4, false)
+	# Dash lasts 0.25 seconds
 	await get_tree().create_timer(0.25).timeout
-	velocity = velocity / DASH_MULTIPLIER
+	is_dashing = false
+	set_collision_mask_value(4, true)
 	
-	
-	var cooldown : float = randf_range(3.0, 6.0)
+	var cooldown = randf_range(3.0, 6.0)
 	await get_tree().create_timer(cooldown).timeout
 	dash_ready = true
-	
-	
 	
 	
 	

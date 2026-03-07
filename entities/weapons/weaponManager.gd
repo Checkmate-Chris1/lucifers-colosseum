14bcc3e84cdb2952b87c53dd767eb29234377311
Harrison Tran <harrisontran357@gmail.com>
2026-03-06 18:52:11 -0800

    collisions don't double up

--- entities/weapons/weaponManager.gd
+++ entities/weapons/weaponManager.gd
@@ -254,13 +254,23 @@ func shoot_ray() -> void:
 	tracer.height = tracer_start.distance_to(end)
 	
 	var collisions = _get_all_enemy_collisions(query, space_state)
-	if current_weapon.penetration:
-		for collider in collisions:
-			collider.hit(current_weapon.bullet_damage)
-			#print("Hit: %s" % collider)
-	elif len(collisions) >= 1:
-		collisions[0].hit(current_weapon.bullet_damage)
-		#print("Hit: %s" % collisions[0])
+	
+	if collisions.size() > 0:
+		var processed_entities = []
+		for hitbox in collisions:
+			var target_entity = hitbox.entity
+			if not target_entity: 
+				continue
+			if target_entity in processed_entities:
+				continue
+			var best_hitbox = hitbox
+			for check_hitbox in collisions:
+				if check_hitbox.entity == target_entity and check_hitbox.damage_multiplier > best_hitbox.damage_multiplier:
+					best_hitbox = check_hitbox
+			best_hitbox.hit(current_weapon.bullet_damage)
+			processed_entities.append(target_entity)
+			if not current_weapon.penetration:
+				break
 
 func _get_all_enemy_collisions(query: PhysicsRayQueryParameters3D, space_state: PhysicsDirectSpaceState3D) -> Array:
 	query.exclude = []
@@ -269,6 +279,7 @@ func _get_all_enemy_collisions(query: PhysicsRayQueryParameters3D, space_state: 
 
 	while ray_result:
 		var collider = ray_result.get("collider")
+		print(collider)
 		if collider is Hitbox:
 			collisions.append(collider)
 			query.exclude.append(collider.get_rid())
